d674a712904db390c26c020f13e3856ece8ed258
karma <karmatashi954@gmail.com>
2022-05-09 11:14:25 +0545

    Initial Commit

---
+++ RockPaperScissorGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorGame
+//
+//  Created by karma on 5/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissorGame/RockPaperScissorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorGameApp.swift
+//  RockPaperScissorGame
+//
+//  Created by karma on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
